be42b871d419c17bff69e5f5e349cb3c364a90ec
Xuwei Li <dev@xuwei.li>
2021-09-11 17:50:13 +0000

    Delete session field from PublicKey model.

--- models/public-key.js
+++ models/public-key.js
@@ -9,11 +9,9 @@
  *   - @param {*} modelName (optional)
  *   - @param {*} env (optional)
  * @param {*} IdentitySettings (optional)
- * @param {*} SessionSettings (optional)
  */
-module.exports = ({ mongoose, modelName, env }, IdentitySettings, SessionSettings) => {
+module.exports = ({ mongoose, modelName, env }, IdentitySettings) => {
   const { Identity } = require('v4ex-api-identity/models/all-identity')(IdentitySettings || {})
-  const { Session } = require('v4ex-api-login/models/session')(SessionSettings || {})
   
   mongoose = mongoose || require('../mongoose')({ env })
   modelName = modelName || 'PublicKey'
@@ -27,8 +25,7 @@ module.exports = ({ mongoose, modelName, env }, IdentitySettings, SessionSetting
     const Schema = mongoose.Schema
     PublicKeySchema = new Schema({
       identity: { type: mongoose.ObjectId, ref: Identity, require: true, immutable: true },
-      key: { type: String, require: true, immutable: true },
-      session: { type: mongoose.ObjectId, ref: Session, unique: true, sparse: true, immutable: true }
+      key: { type: String, require: true, immutable: true }
     })
     PublicKey = mongoose.model(modelName, PublicKeySchema)
   }
